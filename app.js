cf5f00f44f37eb60d21db4942b42254078a4b832
Ryan Hsun <lenxun.huang@gmail.com>
2021-05-04 23:00:13 +0800

    feat: add delete restaurant function & setting

--- app.js
+++ app.js
@@ -108,6 +108,15 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// 設定將特定資料刪除的路由
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect(`/`))
+    .catch(error => console.log(error))
+})
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
   const restaurants = restaurantList.results.filter(restaurant =>
